2eb85e5d75c7b4d311fe4127854202f0789ce562
ldmissjana <>
2023-01-24 13:05:47 +0000

    es

---
+++ main.py
@@ -0,0 +1,14 @@
+def izmaksas_receptei(recepte, cena):
+  summa = 0
+  for sastavdala, daudzums in recepte.items():
+    summa = daudzums * cena[sastavdala]
+  return summa
+
+def izmaksas_kopa(abolu_svars):
+  recepte = {"cukurs": 0.6, "kanelis": 0.08, "aboli": 2.0, "udens": 0.2}
+  cenas = {"cukurs": 0.75, "kanelis": 30.0, "aboli": 0.0, "udens": 0.0}
+  izmaksas_kg = izmaksas_receptei(recepte, cenas) / recepte["aboli"]
+  return abolu_svars * izmaksas_kg
+
+aboli = 1.5
+print("Uz {} kg abolu izmaksas bus {} EUR".format(aboli, izmaksas_kopa(aboli)))
